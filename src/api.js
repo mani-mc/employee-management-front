dc76d1f1b2ccc8d19c9b29d8d57e85dfd6fa7609
Manikandan <manikandan.thedev@gmail.com>
2025-12-11 23:18:46 +0530

    api call fixed

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:5000";
+
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_APP_API_URL || "http://localhost:5000/api/employees",
+  baseURL: `${API_URL}/api/employees`,
 });
 
 export default API;
